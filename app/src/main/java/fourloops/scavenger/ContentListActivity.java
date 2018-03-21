11330f14f0bb60a56fc6807aa20ee4d9ea324f0b
tjk6 <tjk6@students.uwf.edu>
2018-03-21 02:19:35 -0500

    Completed base activities
    Image buttons should be functional.

--- app/src/main/java/fourloops/scavenger/ContentListActivity.java
+++ app/src/main/java/fourloops/scavenger/ContentListActivity.java
@@ -2,7 +2,8 @@ package fourloops.scavenger;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -11,15 +12,29 @@ import java.util.ArrayList;
  */
 
 public class ContentListActivity extends Activity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_list);
 
+        //TODO: You may be able to add a new item to the list each time you update this.
         ArrayList<ListItem> itemList = new ArrayList<>();
         itemList.add(new ListItem("Building", "4")); //Change these to be variable;
         itemList.add(new ListItem("Floor", "3")); //Change these to be variable;
         itemList.add(new ListItem("Room", "304")); //Change these to be variable;
+        itemList.add(new ListItem("Description","STORAGE_ROOM"));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerAdapter = new DataListAdapter(itemList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerAdapter);
+
     }
 }
--- app/src/main/java/fourloops/scavenger/MainActivity.java
+++ app/src/main/java/fourloops/scavenger/MainActivity.java
@@ -3,12 +3,16 @@ package fourloops.scavenger;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private Button qrLaunchButton;
-    private Button qrCreatorButton;
+    private ImageButton qrLaunchButton;
+    private ImageButton qrCreatorButton;
+    private ImageButton pinButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +37,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        pinButton = findViewById(R.id.bldg_info);
+
+        pinButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchBldgInfo();
+            }
+        });
+
     }
 
     private void launchQRScanner(){
+        Log.d("Launcher", "Main button was pressed\n");
         Intent intent = new Intent(this, QRScannerActivity.class);
         startActivity(intent);
     }
 
     private void launchQRCreator(){
+        Log.d("Launcher", "Button was pressed\n");
         Intent intent = new Intent(this, ContentListActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchBldgInfo(){
+        Log.d("Launcher", "Bldg Info button was pressed\n");
+        Intent intent = new Intent(this, BldgMapActivity.class);
         startActivity(intent);
     }
 }
